882132b64a6a6fb2408e6d8f2d4d6a144b726849
abdishukri <abdishukrimohamed105@gmail.com>
2023-02-09 19:21:34 +0300

    passed all tests

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+require "pry"
 
+class Book
+    attr_accessor :author, :page_count, :genre
+   attr_reader :title
 
+   def initialize(title)
+    @title = title
+   end
+   def turn_page
+   puts "Flipping the page...wow, you read fast!"
+   end
+end
+
+atomic = Book.new("habit")
+atomic.author ="james clear"
+atomic.genre = "habit building"
+atomic.page_count = 100
+atomic.turn_page
